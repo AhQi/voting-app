111680fe1d4bb518e4a2e6087a2f3824bec9656d
ahChi <ah941206@gmail.com>
2018-03-08 02:06:32 +0800

    change origin in res header

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 app.use(function(req, res, next) {  
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "https://voting-app-frontend.herokuapp.com");
   res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials");
   res.header("Access-Control-Allow-Credentials", "true");
